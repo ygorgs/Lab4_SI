78258d95375131c80d0759374d2b518999295efc
ivanAguiar <ivan.aguiar.almeida@ccc.ufcg.edu.br>
2014-02-15 22:19:21 -0300

    first commit

---
+++ app/controllers/ListaDeDisciplinas.java
@@ -0,0 +1,79 @@
+package controllers;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
+public class ListaDeDisciplinas {
+
+	private static ListaDeDisciplinas listaDeDisciplinas;
+	private ArrayList<Disciplina> listaDisc;
+
+	// Armazena todas as disciplinas oferecidas pelo curso
+	public ListaDeDisciplinas() {
+		listaDisc = new ArrayList<Disciplina>();
+		try {
+			preencherDisciplinas();
+		} catch (JDOMException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static ListaDeDisciplinas getInstance() {
+		if (listaDeDisciplinas == null) {
+			listaDeDisciplinas = new ListaDeDisciplinas();
+		}
+		return listaDeDisciplinas;
+	}
+
+	/**
+	 * 
+	 * @param disciplina
+	 * @return uma determinada disciplina
+	 */
+	public Disciplina getDisciplina(int index) {
+		return listaDisc.get(index);
+	}
+
+	public Disciplina getDisciplina(String nome) {
+		for (int i = 0; i < listaDisc.size(); i++) {
+			if (listaDisc.get(i).getNome().equals(nome))
+				return listaDisc.get(i);
+		}
+		return null;
+	}
+
+	private void preencherDisciplinas() throws JDOMException, IOException {
+
+		File f = new File("disciplinas-do-curso.xml");
+		SAXBuilder sb = new SAXBuilder();
+
+		Document d = sb.build(f);
+		Element mural = d.getRootElement();
+		List<?> elements = mural.getChildren();
+		Iterator<?> i = elements.iterator();
+
+		while (i.hasNext()) {
+			Element element = (Element) i.next();
+
+			ArrayList<Disciplina> preReq = new ArrayList<Disciplina>();
+			for (Object e1 : element.getChild("requisitos").getChildren()) {
+				preReq.add(listaDisc.get(Integer.parseInt(((Element) e1).getValue())));
+			}
+
+			listaDisc.add(new Disciplina(element.getAttributeValue("id"), element.getAttributeValue("nome"), element
+					.getChildText("creditos"), preReq, element.getChildText("dificuldade")));
+
+		}
+
+	}
+}
---
+++ test/SystemTest.java
@@ -0,0 +1,95 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import controllers.Disciplina;
+import controllers.ListaDeDisciplinas;
+import controllers.Periodo;
+import controllers.PlanoDeCurso;
+
+
+public class SystemTest {
+
+	private ListaDeDisciplinas grade;
+	
+	@Before
+	public void inicializar(){
+		grade = ListaDeDisciplinas.getInstance();
+	}
+	
+	/*
+	 * visito o sistema e ele me exibe uma lista das disciplinas do 1o período já alocadas
+	 */
+	@Test
+	public void deveExibirListaDasDisciplinasDoPrimeiroPeriodo() {
+		PlanoDeCurso meuPlano = new PlanoDeCurso();
+		Periodo primeiroPeriodo = meuPlano.getPeriodo(1);
+		Periodo primeiroPeriodoPadrao = meuPlano.getPeriodoPadrao(1);
+		
+		for (Disciplina disciplina : primeiroPeriodoPadrao.getDisciplinas()) {
+			assertTrue(primeiroPeriodo.getDisciplinas().contains(disciplina));
+		}
+	}
+	
+	/*
+	 * Uma disciplina tem pré-requisitos, e ela só pode ser incluída se seus prérequisitos 
+	 * já foram incluídos no 1o período. 
+	 */
+	@Test
+	public void deveAdicionarDisciplinasComPreRequisitos(){
+		PlanoDeCurso meuPlano = new PlanoDeCurso();
+		assertTrue(meuPlano.addDisciplina(grade.getDisciplina("Programacao 2"),2));
+		assertFalse(meuPlano.addDisciplina(grade.getDisciplina("Estrutura de Dados e Algoritmos"),2));
+	}
+	
+	/*
+	 *  Não é possível incluir disciplinas de forma a ter mais de 28 créditos alocados no período.
+	 */
+	@Test
+	public void naoDeveTerMaisDe28CreditosEmUmPeriodo(){
+		PlanoDeCurso meuPlano = new PlanoDeCurso();
+		meuPlano.addDisciplina(grade.getDisciplina("Programacao 2"),2);
+		meuPlano.addDisciplina(grade.getDisciplina("Laboratório de Programacao 2"),2);
+		meuPlano.addDisciplina(grade.getDisciplina("Calculo Diferencial e Integral 2"),2);
+		meuPlano.addDisciplina(grade.getDisciplina("Matemática Discreta"),2);
+		meuPlano.addDisciplina(grade.getDisciplina("Fundamentos de Física Clássica"),2);
+		meuPlano.addDisciplina(grade.getDisciplina("Metodologia Científica"),2);
+		meuPlano.addDisciplina(grade.getDisciplina("Opitativa"),2);
+		assertFalse(meuPlano.addDisciplina(grade.getDisciplina("Opitativa"),2));
+	}
+
+	/*
+	 * retirar disciplinas alocadas para os períodos a partir do 2o, 
+	 */
+	@Test
+	public void deveRetirarDisciplinasAlocadas(){
+		PlanoDeCurso meuPlano = new PlanoDeCurso();
+		meuPlano.addDisciplina(grade.getDisciplina("Programacao 2"),2);
+		//disciplina do 1º periodo
+		assertFalse(meuPlano.removerDisciplina(grade.getDisciplina("Programacao 1")));
+		//disciplina do 2º periodo
+		assertTrue(meuPlano.removerDisciplina(grade.getDisciplina("Programacao 2")));
+		//disciplina que não está na grade
+		assertFalse(meuPlano.removerDisciplina(grade.getDisciplina("Metodologia Científica")));
+	}
+	
+	/*
+	 * Ao retirar uma disciplina que é prerequisito de outras, o sistema deve remover 
+	 * também todos aquelas que fiquem sem seus prérequisitos satisfeitos.
+	 */
+	@Test
+	public void deveRetirarTodasAsDisciplinasSemPrerequisitosSatisfeitos(){
+		PlanoDeCurso meuPlano = new PlanoDeCurso();
+		meuPlano.addDisciplina(grade.getDisciplina("Programacao 2"),2);
+		meuPlano.addDisciplina(grade.getDisciplina("Laboratório de Programacao 2"),2);
+		meuPlano.addDisciplina(grade.getDisciplina("Teoria dos Grafos"),2);
+		meuPlano.addDisciplina(grade.getDisciplina("Estrutura de Dados e Algoritmos"),3);
+		
+		meuPlano.removerDisciplina((grade.getDisciplina("Teoria dos Grafos")));
+		
+		assertFalse(meuPlano.removerDisciplina((grade.getDisciplina("Estrutura de Dados e Algoritmos"))));
+	}
+	
+}
